10fed9f3ab52fdef4c5e8d69e23519b5f3acdce7
npalak <neha.rai.8620@gmail.com>
2021-09-03 15:48:33 +0530

    apply,faq modal remove and ask button added

--- js/askModal.js
+++ js/askModal.js
@@ -2,21 +2,18 @@
 
 var askModal = document.getElementById("myaskId");
 
-// Get the button that opens the modal
-var btn = document.getElementById("askBtn");
-
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal
-btn.onclick = function () {
-    askModal.style.display = "block";
-};
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
     askModal.style.display = "none";
 };
+
+function test(){
+  askModal.style.display = "block";
+}
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
@@ -24,3 +21,5 @@ window.onclick = function (event) {
     askModal.style.display = "none";
   }
 };
+
+
