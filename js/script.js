f00adcd0981a62090f19caae21445dd5764f9c96
anilkumar708 <anilkumar1995708@gmail.com>
2021-09-14 18:07:48 +0530

    mobile-view changes added

--- js/script.js
+++ js/script.js
@@ -69,3 +69,12 @@ function showSuggestions(list) {
   }
   suggBox.innerHTML = listData;
 }
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
